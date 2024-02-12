2579b4b0969ae8d619c64aba3d8ffc02712a2570
Abhijeet Panda <abhijeetpanda06@gmail.com>
2024-02-12 11:07:09 +0530

    added cloudflare upload functionality

---
+++ src/cloudUpload.ts
@@ -0,0 +1,27 @@
+import { S3 } from "aws-sdk";
+import fs from "fs";
+
+const s3 = new S3({
+    accessKeyId : `${process.env.ACCESS_KEY_ID}`,
+    secretAccessKey : `${process.env.SECRET_ACCESS_KEY}`,
+    endpoint : `${process.env.ENDPOINT}`
+})
+
+export const uploadFile = async (filename:string, localFilePath:string) => {
+    const fileContent = fs.readFileSync(localFilePath);
+
+    try {
+
+        const uploadRes =  await s3.upload({
+            Body : fileContent,
+            Bucket : "deploymint",
+            Key : filename,
+        }).promise();
+    
+        console.log(uploadRes);
+
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import path from "path"
 import { generate } from "./generate";
 import simpleGit from "simple-git";
 import { getAllFiles } from "./getFiles";
+import { uploadFile } from "./cloudUpload";
 
 const app = express();
 app.use(cors())
@@ -24,10 +25,16 @@ app.post("/deploy", async (req,res) => {
 
     console.log(getFiles);
 
+
+    const file = 'E:\\Web Dev Udemy\\DeployMint\\dist\\output\\lsie0v6iou\\src\\App.jsx';
+
+    await uploadFile(file.slice(__dirname.length + 1), file);
+
     res.json({
         "repoId": repoId
     });
 });
+
 
 app.listen(port,() => {
     console.log("App is running on port: "+ port);
