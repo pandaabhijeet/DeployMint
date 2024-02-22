213741d3a730078014da551d965722174345a4b0
Abhijeet Panda <abhijeetpanda06@gmail.com>
2024-02-22 11:31:56 +0530

    added fontend react functionality

--- front-end/src/components/LandingPage.tsx
+++ front-end/src/components/LandingPage.tsx
@@ -1,7 +1,16 @@
-import "./LandingPage.css"
+import { useState } from "react";
+import "./LandingPage.css";
 import { FaGithub } from "react-icons/fa";
+import axios from "axios";
 
-function LandingPage(){
+const BACKEND_UPLOAD_URL = "http://localhost:3000";
+
+function LandingPage() {
+  const [repoUrl, setRepoUrl] = useState("");
+  const [uploadId, setUploadId] = useState("");
+  const [uploading, setUploading] = useState(false);
+  const [deploying, setDeployed] = useState(false);
+
   return (
     <div className="wrapper">
       <form action="">
@@ -9,14 +18,43 @@ function LandingPage(){
         <h2>Deploy your application in one go !</h2>
 
         <div className="input-box">
-          <input type="text" placeholder="Enter your GitHub Repository..." required/>
-          <FaGithub className="icon"/>
+          <input
+            type="text"
+            onChange={(e) => {
+              setRepoUrl(e.target.value);
+            }}
+            placeholder="Enter your GitHub Repository..."
+            required
+          />
+          <FaGithub className="icon" />
         </div>
 
-        <button className="deploy-btn" type="submit">Deploy</button>
+        <button
+          className="deploy-btn"
+          type="submit"
+          onClick={async () => {
+            setUploading(true);
+            const response = await axios.post(`${BACKEND_UPLOAD_URL}/deploy`, {
+              repoUrl: repoUrl,
+            });
+            setUploadId(response.data.id);
+            setUploading(false);
+
+            const interval = setInterval(async () => {
+              const res = await axios.get(`${BACKEND_UPLOAD_URL}/status?id=${response.data.id}`);
+
+              if(res.data.status === "deployed"){
+                clearInterval(interval);
+                setDeployed(true);
+              }
+            }, 3000)
+          
+          }} disabled={uploadId !=="" || uploading}>
+            {uploadId ? `Deploying (${uploadId})` : uploading ? "Uploading..." : "Upload"}
+        </button>
       </form>
     </div>
-  )
+  );
 }
 
 export default LandingPage;
